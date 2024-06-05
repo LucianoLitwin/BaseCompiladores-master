855ac3efb010f17451810fbfba32e5e7aac6abd8
lucianolitwin <lucianocab02@gmail.com>
2024-06-04 21:03:17 -0300

    Achivos listeners, creacion inicial de tabla de simbolos, correccion de problemas con git"

--- src/main/java/compiladores/App.java
+++ src/main/java/compiladores/App.java
@@ -1,10 +1,5 @@
 package compiladores;
 
-//import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -13,12 +8,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-        
-// Las diferentes entradas se explicaran oportunamente
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Compilador!!!");
-        String inputFile = "input/entrada.txt";
+        System.out.println("Hola Profe! :)");
+        String inputFile = "input/programa.txt";
         String content = new String(Files.readAllBytes(Paths.get(inputFile)));
 
         compiladoresLexer lexer = new compiladoresLexer(CharStreams.fromString(content));
@@ -38,6 +31,5 @@ public class App {
                 System.out.println(error);
             }
         }
-        
     }
 }
